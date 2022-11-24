1cbff8be06de91dce8a2eda9bd6406fc5776da0c
mrawal <imsr1013@gmail.com>
2022-11-24 12:56:52 +0545

    inherit from Item class

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ class Item:
     pay_rate = 0.8
     all = []
 
-    def __init__(self,name: str,price: float,quantity=0):
-        #run validations to received arguments
-        assert price >= 0,f"Price {price} is not greater than zero!"
-        assert quantity >= 0,f"Quantity {quantity} is not greater than zero!"
+    def __init__(self, name: str, price: float, quantity=0):
+        # run validations to received arguments
+        assert price >= 0, f"Price {price} is not greater than zero!"
+        assert quantity >= 0, f"Quantity {quantity} is not greater than zero!"
 
-        #Assign to self object
+        # Assign to self object
         self.name = name
         self.price = price
         self.quantity = quantity
@@ -18,16 +18,16 @@ class Item:
         Item.all.append(self)
 
     def calculate_total_price(self):
-        return self.price*self.quantity
+        return self.price * self.quantity
 
     def apply_discount(self):
-        self.price=self.price*self.pay_rate
+        self.price = self.price * self.pay_rate
 
     @classmethod
     def instantiate_from_csv(cls):
-        with open('items.csv','r') as f:
+        with open('items.csv', 'r') as f:
             reader = csv.DictReader(f)
-            print(reader,'print reader')
+            print(reader, 'print reader')
             items = list(reader)
 
         for item in items:
@@ -41,24 +41,37 @@ class Item:
     def is_integer(num):
         # we will count out the floats that are point zero
         # for i.e 5.0,10.0
-        if isinstance(num,float):
+        if isinstance(num, float):
             # count out the floats that are point zero
             return num.is_integer()
-        elif isinstance(num,int):
+        elif isinstance(num, int):
             return True
         else:
 
             return False
 
-
     def __repr__(self):
         return f"Item('{self.name}',{self.price},{self.quantity})"
 
-phone1 = Item("jscPhonev10",500,5)
-phone1.broken_phones=1
-phone2 = Item("jscPhonev20",700,5)
-phone2.broken_phones=1
+
+class Phone(Item):
+    all = []
+
+    def __init__(self, name: str, price: float, quantity=0, broken_phones=0):
+        # run validations to received arguments
+        assert price >= 0, f"Price {price} is not greater than zero!"
+        assert quantity >= 0, f"Quantity {quantity} is not greater than zero!"
+        assert broken_phones >= 0, f"Broken Phone {broken_phones} is not greater than zero!"
+
+        # Assign to self object
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.broken_phones = broken_phones
+
+        Phone.all.append(self)
 
 
-
-
+phone1 = Phone("jscPhonev10", 500, 5, 2)
+print(phone1.calculate_total_price())
+phone2 = Phone("jscPhonev20", 700, 5, 1)
