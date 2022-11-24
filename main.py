a3a292af4389d2a76836006cd151cae1a3333256
mrawal <imsr1013@gmail.com>
2022-11-24 12:57:54 +0545

    call to super function to have access to all attributes/methods

--- main.py
+++ main.py
@@ -58,15 +58,13 @@ class Phone(Item):
     all = []
 
     def __init__(self, name: str, price: float, quantity=0, broken_phones=0):
-        # run validations to received arguments
-        assert price >= 0, f"Price {price} is not greater than zero!"
-        assert quantity >= 0, f"Quantity {quantity} is not greater than zero!"
+        # Call to super function to have access to all attributes / methods
+        super().__init__(
+            name,price,quantity
+        )
         assert broken_phones >= 0, f"Broken Phone {broken_phones} is not greater than zero!"
 
         # Assign to self object
-        self.name = name
-        self.price = price
-        self.quantity = quantity
         self.broken_phones = broken_phones
 
         Phone.all.append(self)
